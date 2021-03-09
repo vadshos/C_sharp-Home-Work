a5c06f7dc92612ab7dd4d2d0f434157d30a5cace
vadshos <66492574+vadshos@users.noreply.github.com>
2021-03-09 07:23:16 +0200

    Update Program.cs

--- Class Work 3/Class Work 3/Program.cs
+++ Class Work 3/Class Work 3/Program.cs
@@ -8,16 +8,15 @@ namespace Class_Work_3
         static void Main(string[] args)
         {
             Client client = new Client("Petro", "+380681616379");
-
-            Event @event  = new Event(client, "Birthday", "Restorans Afina", new DateTime(2021, 9, 9,13,8,0), 60);
-            Event @event1 = new Event(client, "Birthday", "Restorans Afin", new DateTime(2021, 8, 9,18,0,0), 60);
+            Event event1 = new Event(client, "Birthday", "Restorans Afin", new DateTime(2021, 8, 9,18,0,0), 60);
+            Event event2  = new Event(client, "Birthday", "Restorans Afina", new DateTime(2021, 9, 9,13,8,0), 60);
             EventService eventService = new EventService();
-            eventService.AddEvents(@event);
-            eventService.AddEvents(@event1);
+            eventService.AddEvents(event2);
+            eventService.AddEvents(event1);
             eventService.Print();
             eventService.RemoveEvent(event1.TimeOfHolding,event1.Venue);
             eventService.Print();
-            eventService.findAndEventForSomeDate(@event.TimeOfHolding);
+            eventService.findAndEventForSomeDate(event2.TimeOfHolding);
         }
     }
 }
